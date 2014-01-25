518cf83e6441d7acda44cf1e0138ff0f6a74e1d5
Brandon Valosek <bvalosek@gmail.com>
2014-01-24 22:55:21 -0600

    Fixed shooks formatting

--- game/maps/level-1.js
+++ game/maps/level-1.js
@@ -1,12 +1,12 @@
 module.exports = {
-	walls: [
-		{
-			position : {x: 0, y: 0},
-			spatial: {x : 1, y: 2}
-		},
-		{
-			position : {x: 2, y:2},
-			spatial: {x : 2, y: 1}
-		}
-	]
+  walls: [
+    {
+      position : {x: 0, y: 0},
+      spatial: {x : 1, y: 2}
+    },
+    {
+      position : {x: 2, y:2},
+      spatial: {x : 2, y: 1}
+    }
+  ]
 }
