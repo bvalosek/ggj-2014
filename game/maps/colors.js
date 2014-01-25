687cfc56d7d4c3fdc3183a2a6c22756846489822
Katie Ensign <katie.ensign@gmail.com>
2014-01-25 01:33:26 -0600

    blue update

--- game/maps/colors.js
+++ game/maps/colors.js
@@ -3,7 +3,7 @@ module.exports = {
   orange: { r: 232, g: 134, b: 84  },
   yellow: { r: 255, g: 226, b: 108 },
   green:  { r: 136, g: 233, b: 128 },
-  blue:   { r: 047, g: 255, b: 191 },
+  blue:   { r: 055, g: 180, b: 255 },
   purple: { r: 190, g: 112, b: 255 },
   black:  { r: 0,   g: 0,   b: 0   },
   white:  { r: 255, g: 255, b: 255 }
