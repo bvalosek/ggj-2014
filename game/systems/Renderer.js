b665d1188d77766c429e6ec9974bf3419611a0b7
Brandon Valosek <bvalosek@gmail.com>
2014-01-25 01:56:29 -0600

    Gems as circs

--- game/systems/Renderer.js
+++ game/systems/Renderer.js
@@ -4,6 +4,7 @@ var EntityManager = require('tiny-ecs').EntityManager;
 var Canvas        = require('../../lib/renderer/Canvas.js');
 var Position      = require('../components/Position.js');
 var Spatial       = require('../components/Spatial.js');
+var LevelObject   = require('../components/LevelObject.js');
 
 /**
  * @constructor
@@ -31,16 +32,34 @@ Renderer.prototype.update = function(dt, time)
   for (var n = 0; n < entities.length; n++) {
     var entity = entities[n];
 
-    if (entity.colorSpirit) {
-      screen
-        .save()
-        .vtranslate(entity.position.location)
-        .drawHwRect(entity.spatial.hwidth,
-          entity.colorSpirit.style)
-        .restore();
+    if (entity.levelObject) {
+      switch(entity.levelObject.type) {
+        case LevelObject.types.GEM:
+          this.drawGem(entity);
+        break;
+      }
+    } else if (entity.colorSpirit) {
+      this.drawWall(entity);
     }
-
   }
+};
+
+Renderer.prototype.drawGem = function(entity)
+{
+  this.screen
+    .save()
+    .vtranslate(entity.position.location)
+    .drawCircle(entity.spatial.hwidth.x, entity.colorSpirit.style)
+    .restore();
+};
+
+Renderer.prototype.drawWall = function(entity)
+{
+  this.screen
+    .save()
+    .vtranslate(entity.position.location)
+    .drawHwRect(entity.spatial.hwidth, entity.colorSpirit.style)
+    .restore();
 };
 
 
