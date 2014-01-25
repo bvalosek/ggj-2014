3e8e18afc9827b71c42a8992916452de262c2603
Brandon Valosek <bvalosek@gmail.com>
2014-01-25 00:41:02 -0600

    color changes

--- game/maps/colors.js
+++ game/maps/colors.js
@@ -1,12 +1,10 @@
 module.exports = {
-  colors: {
-    red:    { r: 255, g: 85,  b: 83  },
-    orange: { r: 232, g: 134, b: 84  },
-    yellow: { r: 255, g: 226, b: 108 },
-    green:  { r: 136, g: 233, b: 128 },
-    blue:   { r: 047, g: 255, b: 191 },
-    purple: { r: 190, g: 112, b: 255 },
-    black:  { r: 0,   g: 0,   b: 0   },
-    white:  { r: 255, g: 255, b: 255 }
-  }
+  red:    { r: 255, g: 85,  b: 83  },
+  orange: { r: 232, g: 134, b: 84  },
+  yellow: { r: 255, g: 226, b: 108 },
+  green:  { r: 136, g: 233, b: 128 },
+  blue:   { r: 047, g: 255, b: 191 },
+  purple: { r: 190, g: 112, b: 255 },
+  black:  { r: 0,   g: 0,   b: 0   },
+  white:  { r: 255, g: 255, b: 255 }
 };
