d389f306b49843d9cf8a37d110615391e55563ae
Katie Ensign <katie.ensign@gmail.com>
2014-01-25 00:35:41 -0600

    testing colors

--- game/maps/level-1.js
+++ game/maps/level-1.js
@@ -1,16 +1,15 @@
-var GREEN = { r: 0, g: 255, b: 0 };
-
+var colors = require('./colors.js');
 module.exports = {
   walls: [
     {
       position : {x: 500, y: 500},
       spatial: {x : 100, y: 200},
-      color: {r: 255, g: 0, b: 0}
+      color: colors.blue
     },
     {
       position : {x: 500, y:200},
       spatial: {x : 200, y: 100},
-      color: GREEN
+      color: colors.green
     }
   ]
 };
