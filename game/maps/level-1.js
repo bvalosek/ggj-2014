80d1b064d9755387c0b7d723221785e9f3adfdcb
Brandon Valosek <bvalosek@gmail.com>
2014-01-24 23:41:55 -0600

    color constant example

--- game/maps/level-1.js
+++ game/maps/level-1.js
@@ -1,14 +1,16 @@
+var GREEN = { r: 0, g: 255, b: 0 };
+
 module.exports = {
   walls: [
     {
       position : {x: 500, y: 500},
       spatial: {x : 100, y: 200},
-      colorSpirit: {red: 255, green: 0, blue: 0}
+      color: {red: 255, green: 0, blue: 0}
     },
     {
       position : {x: 20, y:200},
       spatial: {x : 200, y: 100},
-      colorSpirit: {red: 0, green: 255, blue: 0}
+      color: GREEN
     }
   ]
 };
