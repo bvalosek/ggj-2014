7d90bd93ffbefca22395c51cba65346d23923ae2
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-25 09:04:36 -0600

    new poly create

--- game/maps/nicklevel-1.js
+++ game/maps/nicklevel-1.js
@@ -3,10 +3,34 @@ var colors = require('./colors.js');
 var mapCX = 1000;
 var mapCY = 1000;
 
+function createPoly(cX, cY, w, h)
+{
+return [{
+      position : {x: cX, y: cY - (h/2)},
+      spatial: {x : w/2 + C/2, y: C},
+      color: colors.green
+    },
+    {
+      position : {x: cX-(w/2), y: cY},
+      spatial: {x : C, y: h/2 + C},
+      color: colors.green
+    },
+    {
+      position : {x: cX, y: cY +(h/2)},
+      spatial: {x : w/2 + C/2, y: C},
+      color: colors.green
+    },
+    {
+      position : {x: cX +(w/2), y: cY},
+      spatial: {x : C, y: h/2 + C},
+      color: colors.green
+    }];
+}
+
 module.exports = {
   levelObjects:{
     playerStart: {x: C + 25, y: 50 + C},
-    levelFinish:{x: 1000, y: 1000}
+    levelFinish:{x: (3*(mapCX/4)), y: (mapCY/3)}
   },
   gems: [
     //GEM 1
@@ -37,28 +61,5 @@ module.exports = {
       position : {x: mapCX/2, y: mapCY/2},
       spatial: {x : 1*C, y: mapCY/2},
       color: colors.blue
-    },
-    //SQUARE
-    {
-      position : {x: (3*(mapCX/4)), y: (mapCY/3)},
-      spatial: {x : 6*C, y: 1*C},
-      color: colors.green
-    },
-    {
-      position : {x: (3*(mapCX/4))-7*C, y: (mapCY/3)+6*C},
-      spatial: {x : 1*C, y: 7*C},
-      color: colors.green
-    },
-    {
-      position : {x: (3*(mapCX/4)), y: (mapCY/3)+12*C},
-      spatial: {x : 6*C, y: 1*C},
-      color: colors.green
-    },
-    {
-      position : {x: (3*(mapCX/4))+7*C, y: (mapCY/3)+6*C},
-      spatial: {x : 1*C, y: 7*C},
-      color: colors.green
-    },
-
-  ]
+    }].concat( createPoly((3*(mapCX/4)),(mapCY/3), 12*C, 12*C))  
 };
