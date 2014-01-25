2414f3ef7054f2c6211fb665c147c28a5bcdce05
Brandon Valosek <bvalosek@gmail.com>
2014-01-25 02:02:02 -0600

    player start + whitespace cleanup

--- game/maps/nicklevel-1.js
+++ game/maps/nicklevel-1.js
@@ -1,44 +1,44 @@
-var C = 15; 
+var C = 15;
 var colors = require('./colors.js');
 var mapCX = 1000;
 var mapCY = 1000;
 
 module.exports = {
   levelObjects:{
-  	playerStart: {x: 50, y: 60},
-  	levelFinish:{x: 1000, y: 1000}
+    playerStart: {x: C + 25, y: 50 + C},
+    levelFinish:{x: 1000, y: 1000}
   },
   gems: [
-//GEM 1 
+    //GEM 1
     {
       position : {x: C + 25, y:(mapCY/4) * 3},
       color: colors.red
     },
-//GEM 2
+    //GEM 2
     {
       position : {x: 3*(mapCX/8), y:50 + C},
       color: colors.blue
     },
-//GEM 3
+    //GEM 3
     {
       position : {x: (3*(mapCX/4)) + C, y:(mapCY/4) * 3},
       color: colors.green
     }
   ],
   walls: [
-//LINE 1
-    {	
+    //LINE 1
+    {
       position : {x: mapCX/4, y: mapCY/2},
       spatial: {x : 1*C, y: mapCY/2},
-      color: colors.red 
+      color: colors.red
     },
-//LINE 2
-    {	
+    //LINE 2
+    {
       position : {x: mapCX/2, y: mapCY/2},
       spatial: {x : 1*C, y: mapCY/2},
       color: colors.blue
     },
-//SQUARE
+    //SQUARE
     {
       position : {x: (3*(mapCX/4)), y: (mapCY/3)},
       spatial: {x : 6*C, y: 1*C},
@@ -60,11 +60,5 @@ module.exports = {
       color: colors.green
     },
 
-//START
-    {
-      position : {x: C + 25, y:50 + C},
-      spatial: {x : C, y: C},
-      color: colors.green
-    },
   ]
 };
