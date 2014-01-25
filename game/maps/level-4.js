d284e02fde28a0f80e336251cb82a3605b800d40
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-25 17:44:53 -0600

    simplify walls in 4 add create poly method to simplify

--- game/maps/level-4.js
+++ game/maps/level-4.js
@@ -4,50 +4,14 @@ var mapCX = 1000;
 var mapCY = 1000;
 var unit = mapCX/30;
 
-function u(n) { return unit*n; }
-
-function createPoly(cX, cY, w, h, color, b1)
-{
-var retArray = [];
-if(b1[0])
-{
-	retArray=retArray.concat([{
-      position : {x: cX, y: cY - (h/2)},
-      spatial: {x : w/2 , y: C},
-      color: color[0]
-    }]);
-}if(b1[1])
-{
-
-	retArray=retArray.concat([{
-      position : {x: cX +(w/2) + C, y: cY},
-      spatial: {x : C, y: h/2 + C},
-      color: color[1]
-    }]);
-}
-if(b1[2])
-{
-	retArray=retArray.concat([{
-      position : {x: cX, y: cY +(h/2)},
-      spatial: {x : w/2, y: C},
-      color: color[2]
-    }]);
-}
-if(b1[3])
-{
-	retArray=retArray.concat( [  {
-      position : {x: cX-(w/2)-C, y: cY},
-      spatial: {x : C, y: h/2 + C},
-      color: color[3]
-    }]);
-}
-return retArray;
-}
+var u = require('./simplify.js').u;
+var line = require('./simplify.js').line;
+var createPoly = require('./simplify.js').createPoly;
 
 module.exports = {
   levelObjects:{
-    playerStart: {x: C + 100, y: 50 + C},
-    levelFinish:{x: (3*(mapCX/4)), y: (mapCY/3)}
+    playerStart: {x: 100, y: 50},
+    levelFinish:{x: 500, y: 500}
   },
   gems: [
   {
@@ -132,114 +96,31 @@ module.exports = {
   }
 
   ],
-  walls: [
-    {
-      position : {x:u(6), y: u(17)},
-      spatial: {x : C, y: u(8)},
-      color: colors.green,
-	text: 'g1'
-    },
-    {
-      position : {x:u(6), y: u(4)},
-      spatial: {x : C, y: u(5)},
-      color: colors.yellow,
-	text: 'o1'
-    },
-    {
-      position : {x:u(12)+C, y: u(12)},
-      spatial: {x : C, y: u(12)},
-      color: colors.red,
-	text:'r1'
-    },
-    {
-      position : {x:u(18), y: u(18)},
-      spatial: {x : C, y: u(6)},
-      color: colors.yellow,
-	text: 'o2'
-    },
-    {
-      position : {x:u(18), y: u(6)},
-      spatial: {x : C, y: u(6)},
-      color: colors.purple,
-	text:'p1'
-    },
-    {
-      position : {x:u(22), y: u(4)},
-      spatial: {x : C, y: u(4)},
-      color: colors.blue,
-	text:'b3'
-    },
-    {
-      position : {x:u(26), y: u(8)-C},
-      spatial: {x : u(4), y: C},
-      color: colors.blue,
-	text:'b4'
-    },
-    {
-      position : {x:u(22), y: u(10)},
-      spatial: {x : C, y: u(2)},
-      color: colors.green
-    },
-    {
-      position : {x:u(20)+C, y: u(12)},
-      spatial: {x : u(2), y: C},
-      color: colors.blue,
-	text:'b5'
-    },
-    {
-      position : {x:u(26), y: u(12)},
-      spatial: {x : u(4), y: C},
-      color: colors.purple,
-	text:'p3'
-    },
-    {
-      position : {x:u(25), y: u(3)},
-      spatial: {x : C, y: u(3)},
-      color: colors.green,
-	text:'g2'
-    },
-    {
-      position : {x:u(28), y: u(6)-C},
-      spatial: {x : u(3), y: C},
-      color: colors.green,
-	text:'g3'
-    },
-    {
-      position : {x:u(32)+C, y: u(6)-C},
-      spatial: {x : u(2), y: C},
-      color: colors.purple,
-	text:'p4'
-    },
-    {
-      position : {x:u(30), y: u(15)},
-      spatial: {x : C, y: u(9)},
-      color: colors.red,
-	text: 'r3'
-    },
-    {
-      position : {x:u(24)+C, y: u(17)},
-      spatial: {x : u(6), y: C},
-      color: colors.green,
-	text:'g4'
-    },
-    {
-      position : {x:u(32)+C, y: u(8)-C},
-      spatial: {x : u(2), y: C},
-      color: colors.green,
-	text:'g5'
-    },
-    {
-      position : {x:u(32)+C, y: u(12)},
-      spatial: {x : u(2), y: C},
-      color: colors.blue,
-	text:'b6'
-    }
-].concat(createPoly(u(9), u(13), u(6), u(8), 
+  walls: [].
+concat(line(u(24)+C,u(17),u(6),C,colors.green,'g4')).
+concat(line(u(30),u(15),C,u(9),colors.red,'r3')).
+concat(line(u(32)-C,u(12),u(2),C,colors.blue,'b6')).
+concat(line(u(26),u(12),u(4),C,colors.purple,'p3')).
+concat(line(u(32)-C,u(8)-C,u(2),C,colors.green,'g5')).
+concat(line(u(20)+C,u(12),u(2),C,colors.blue,'b5')).
+concat(line(u(26),u(8)-C,u(4),C,colors.blue,'b4')).
+concat(line(u(22),u(4),C,u(4),colors.blue,'b3')).
+concat(line(u(22),u(10)-C/2,C,u(2)-C/2,colors.green,'g6')).
+concat(line(u(28),u(6)-C,u(3),C,colors.green,'g3')).
+concat(line(u(25),u(3),C,u(3),colors.green,'g2')).
+concat(line(u(6),u(17),C,u(8),colors.green,'g1')).
+concat(line(u(32)+C,u(6)-C,u(2),C,colors.purple,'p4')).
+concat(line(u(18),u(6),C,u(6),colors.purple,'p1')).
+concat(line(u(6),u(4),C,u(5),colors.yellow,'y1')).
+concat(line(u(18),u(18),C,u(6),colors.yellow,'y2')).
+concat(line(u(12)+C,u(12),C,u(12),colors.red,'r1')).
+	concat(createPoly(u(9), u(13), u(6), u(8), 
 	[colors.blue,colors.yellow,colors.purple,colors.red], 
-	[1,0,1,0] ) ).concat(
+	[1,0,1,0], C ) ).
+	concat(
 	createPoly(u(3), u(13), u(6), u(8), 
 	[colors.red,null,colors.blue,null], 
-	[true,false,true,false] ) 
+	[true,false,true,false], C ) 
 	)
 
 };
--- game/maps/simplify.js
+++ game/maps/simplify.js
@@ -22,10 +22,51 @@ function createGem(posx,posy,col,text)
 	text: text
     	}];
 }
+
+
+function createPoly(cX, cY, w, h, color, b1, C)
+{
+var retArray = [];
+if(b1[0])
+{
+	retArray=retArray.concat([{
+      position : {x: cX, y: cY - (h/2)},
+      spatial: {x : w/2 , y: C},
+      color: color[0]
+    }]);
+}if(b1[1])
+{
+
+	retArray=retArray.concat([{
+      position : {x: cX +(w/2) + C, y: cY},
+      spatial: {x : C, y: h/2 + C},
+      color: color[1]
+    }]);
+}
+if(b1[2])
+{
+	retArray=retArray.concat([{
+      position : {x: cX, y: cY +(h/2)},
+      spatial: {x : w/2, y: C},
+      color: color[2]
+    }]);
+}
+if(b1[3])
+{
+	retArray=retArray.concat( [  {
+      position : {x: cX-(w/2)-C, y: cY},
+      spatial: {x : C, y: h/2 + C},
+      color: color[3]
+    }]);
+}
+return retArray;
+}
+
 module.exports = { 
 	u: u,
 	line: line,
 	createGem: createGem,
 	mapCX: mapCX,
-	unit: unit
+	unit: unit,
+	createPoly: createPoly
 }
