6dcd7ee9281e9213c223ca2e8295a76341cf9e11
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-25 10:18:29 -0600

    level 3 more walls

--- game/maps/level-3.js
+++ game/maps/level-3.js
@@ -25,27 +25,70 @@ module.exports = {
     {
       position : {x: u(2), y: u(2)},
       spatial: {x : C, y: u(2)},
-      color: colors.green
+      color: colors.green,
+      text: 'g1'
     },
     {
-      position : {x: u(6) - C, y: u(4)},
-      spatial: {x : u(4), y: C },
-      color: colors.red
+      position : {x: u(6), y: u(4)},
+      spatial: {x : u(4) + C, y: C },
+      color: colors.red,
+	text: 'r1'
     },
     {
       position : {x: u(5) - C, y: u(7)+C},
       spatial: {x : C, y: u(3) },
-      color: colors.blue
+      color: colors.blue,
+	text: 'b1'
     },
     {
       position : {x: u(2), y: u(10) - C},
       spatial: {x : u(3), y: C },
-      color: colors.red
+      color: colors.red,
+	text: 'r2'
     },
     {
       position : {x: u(5) - C, y: u(12)},
       spatial: {x : C, y: u(2) },
-      color: colors.purple
+      color: colors.purple,
+	text: 'p1'
     },
+    {
+      position : {x: u(10), y: u(7)+C},
+      spatial: {x : C, y: u(3) },
+      color: colors.purple,
+	text: 'p2'
+    },
+    {
+      position : {x: u(12)+C, y: u(10)},
+      spatial: {x : u(2), y: C },
+      color: colors.green,
+	text: 'g2'
+    },
+    {
+      position : {x: u(14)+C, y: u(1)},
+      spatial: {x : C, y: u(2) },
+      color: colors.purple,
+	text: 'p3'
+    },
+
+    {
+      position : {x: u(14)+C, y: u(6)+C},
+      spatial: {x : C, y: u(4) },
+      color: colors.blue,
+	text: 'b2'
+    },
+    {
+      position : {x: u(15), y: u(10)},
+      spatial: {x : u(1), y: C },
+      color: colors.red,
+	text: 'r3'
+    },
+    {
+      position : {x: u(16), y: u(12)-C},
+      spatial: {x : C, y: u(2) },
+      color: colors.green,
+	text: 'g3'
+    },
+
 ]  
 };
