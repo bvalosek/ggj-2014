673909aafa5b63554a3411ea0a6f4ecdb03ab331
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-25 23:48:32 -0600

    Level 3 rework

--- game/maps/level-3.js
+++ game/maps/level-3.js
@@ -1,145 +1,42 @@
 var C = 10;
 var colors = require('./colors.js');
 var mapCX = 1000;
-var mapCY = 1000;
-var unit = mapCX/20;
+var mapCY = 710;
 
-function u(n) { return unit*n; }
+var u = require('./simplify.js').u;
+var line = require('./simplify.js').line;
+var createGem = require('./simplify.js').createGem;
 
 module.exports = {
   levelObjects:{
     playerStart: {x: u(12)+C, y: u(8)},
-    levelFinish:{x: u(22), y: u(12)-C}
+    levelFinish:{x: u(22), y: u(16)}
   },
   size: { x: mapCX, y: mapCY },
   startColor: colors.yellow,
-  gems: [
-    {
-      position : {x: u(3), y:u(5)},
-      color: colors.red
-    },
-    {
-      position : {x: u(2), y: u(12)},
-      color: colors.purple
-    },
-    {
-      position : {x: u(17), y: u(12)-C},
-      color: colors.green
-    },
-    {
-	position : {x: u(16), y: u(1)},
-	color: colors.red
-    },
-    {
-	position : {x: u(12)+C, y: u(1)},
-	color: colors.blue
-    }
-  ],
-  walls: [
-    {
-      position : {x: u(2), y: u(2)},
-      spatial: {x : C, y: u(2)},
-      color: colors.green,
-      text: 'g1'
-    },
-    {
-      position : {x: u(6), y: u(4)},
-      spatial: {x : u(4) + C, y: C },
-      color: colors.red,
-	text: 'r1'
-    },
-    {
-      position : {x: u(5) - C, y: u(7)+C},
-      spatial: {x : C, y: u(3) },
-      color: colors.blue,
-	text: 'b1'
-    },
-    {
-      position : {x: u(2), y: u(10) - C},
-      spatial: {x : u(3), y: C },
-      color: colors.red,
-	text: 'r2'
-    },
-    {
-      position : {x: u(5) - C, y: u(12)},
-      spatial: {x : C, y: u(2) },
-      color: colors.purple,
-	text: 'p1'
-    },
-    {
-      position : {x: u(10), y: u(7)+C},
-      spatial: {x : C, y: u(3) },
-      color: colors.purple,
-	text: 'p2'
-    },
-    {
-      position : {x: u(12)+C, y: u(10)},
-      spatial: {x : u(2), y: C },
-      color: colors.green,
-	text: 'g2'
-    },
-    {
-      position : {x: u(14)+C, y: u(1)},
-      spatial: {x : C, y: u(2) },
-      color: colors.purple,
-	text: 'p3'
-    },
-
-    {
-      position : {x: u(14)+C, y: u(6)+C},
-      spatial: {x : C, y: u(4) },
-      color: colors.blue,
-	text: 'b2'
-    },
-    {
-      position : {x: u(15), y: u(10)},
-      spatial: {x : u(1), y: C },
-      color: colors.red,
-	text: 'r3'
-    },
-    {
-      position : {x: u(16), y: u(12)-C},
-      spatial: {x : C, y: u(2) },
-      color: colors.green,
-	text: 'g3'
-    },
-    {
-      position : {x: u(22), y: u(10)},
-      spatial: {x : u(2), y: C },
-      color: colors.blue,
-	text: 'b3'
-    },
-    {
-      position : {x: u(16)+C/2, y: u(2)},
-      spatial: {x : u(2)+C/2, y: C },
-      color: colors.green,
-	text: 'r5'
-    },
-    {
-      position : {x: u(19), y: u(4)},
-      spatial: {x : u(1), y: C },
-      color: colors.purple,
-	text: 'p5'
-    },
-    {
-      position : {x: u(18), y: u(3)+C},
-      spatial: {x : C, y: u(1) },
-      color: colors.red,
-	text: 'r5'
-    },
-    {
-      position : {x: u(20), y: u(7)-C},
-      spatial: {x : C, y: u(3) },
-      color: colors.red,
-	text: 'r4'
-    },
-    {
-      position : {x: u(20), y: u(12)-C},
-      spatial: {x : C, y: u(2) },
-      color: colors.purple,
-	text: 'p4'
-    },
-
-
-]  
+  gems: [].
+concat(createGem( u(3),u(9), colors.red)).
+concat(createGem( u(2),u(16), colors.purple)).
+concat(createGem( u(17),u(16)-C, colors.green)).
+concat(createGem( u(16),u(1), colors.red)).
+concat(createGem( u(12)+C,u(1), colors.blue))
+,
+  walls: [].
+concat(line(u(2),u(4),C,u(4),colors.green, 'g1')).
+concat( line(u(6),u(8),u(4)+C,C,colors.red, 'r1')).
+concat( line(u(2.5),u(14),u(2.5),C,colors.red, 'r2')).
+concat( line(u(5)-C,u(11)+C,C,u(3),colors.blue, 'b1')).
+concat( line(u(5)-C,u(16)-C,C,u(2),colors.purple, 'p1')).
+concat( line(u(10),u(11)+C,C,u(3),colors.purple, 'p2')).
+concat( line(u(12)+C,u(14),u(2),C,colors.green, 'g2')).
+concat( line(u(14)+C,u(2),C,u(2),colors.purple, 'p3')).
+concat( line(u(14)+C,u(9),C,u(5),colors.blue, 'b2')).
+concat( line(u(15),u(14),u(1),C,colors.red, 'r3')).
+concat( line(u(16),u(16)-C,C,u(2),colors.green, 'g3')).
+concat( line(u(22),u(14),u(2), C,colors.blue, 'b3')).
+concat( line(u(16)+C/2,u(4),u(2)+C/2, C,colors.green, 'g4')).
+concat( line(u(19),u(6),u(1),C,colors.purple, 'p5')).
+concat( line(u(18),u(5)+C,C,u(1),colors.red, 'r5')).
+concat( line(u(20),u(10)-C,C,u(4),colors.red, 'r4')).
+concat( line(u(20),u(16)-C,C,u(2),colors.purple, 'p6'))  
 };
