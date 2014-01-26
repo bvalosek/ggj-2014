f2b3b401727cd6ab41795d5ba1b54a6f61f48732
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-26 00:01:15 -0600

    Level 3 cleanup/rework

--- game/maps/level-3.js
+++ game/maps/level-3.js
@@ -10,14 +10,14 @@ var createGem = require('./simplify.js').createGem;
 module.exports = {
   levelObjects:{
     playerStart: {x: u(12)+C, y: u(8)},
-    levelFinish:{x: u(22), y: u(16)}
+    levelFinish:{x: u(23), y: u(16)}
   },
   size: { x: mapCX, y: mapCY },
   startColor: colors.yellow,
   gems: [].
-concat(createGem( u(3),u(9), colors.red)).
+concat(createGem( u(3),u(11), colors.red)).
 concat(createGem( u(2),u(16), colors.purple)).
-concat(createGem( u(17),u(16)-C, colors.green)).
+concat(createGem( u(18),u(16)-C, colors.green)).
 concat(createGem( u(16),u(1), colors.red)).
 concat(createGem( u(12)+C,u(1), colors.blue))
 ,
@@ -30,13 +30,13 @@ concat( line(u(5)-C,u(16)-C,C,u(2),colors.purple, 'p1')).
 concat( line(u(10),u(11)+C,C,u(3),colors.purple, 'p2')).
 concat( line(u(12)+C,u(14),u(2),C,colors.green, 'g2')).
 concat( line(u(14)+C,u(2),C,u(2),colors.purple, 'p3')).
-concat( line(u(14)+C,u(9),C,u(5),colors.blue, 'b2')).
+concat( line(u(14)+C,u(9)+C/2,C,u(5)+C/2,colors.blue, 'b2')).
 concat( line(u(15),u(14),u(1),C,colors.red, 'r3')).
 concat( line(u(16),u(16)-C,C,u(2),colors.green, 'g3')).
-concat( line(u(22),u(14),u(2), C,colors.blue, 'b3')).
+concat( line(u(23),u(14),u(2), C,colors.blue, 'b3')).
 concat( line(u(16)+C/2,u(4),u(2)+C/2, C,colors.green, 'g4')).
-concat( line(u(19),u(6),u(1),C,colors.purple, 'p5')).
-concat( line(u(18),u(5)+C,C,u(1),colors.red, 'r5')).
-concat( line(u(20),u(10)-C,C,u(4),colors.red, 'r4')).
-concat( line(u(20),u(16)-C,C,u(2),colors.purple, 'p6'))  
+concat( line(u(20)-C,u(7),u(1.5),C,colors.purple, 'p5')).
+concat( line(u(18),u(5.5)+C,C,u(1.5),colors.red, 'r5')).
+concat( line(u(21),u(11)-C,C,u(3.5),colors.red, 'r4')).
+concat( line(u(21),u(16)-C,C,u(2),colors.purple, 'p6'))  
 };
