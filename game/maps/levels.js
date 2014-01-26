774db23602aedd03c4e652e8b9b333cca40a41d5
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-26 13:26:08 -0600

    renumber

--- game/maps/levels.js
+++ game/maps/levels.js
@@ -1,12 +1,12 @@
 module.exports = {
-  level0    : require('./level-0.js'),
-  level1    : require('./nicklevel-1.js'),
-  level2    : require('./level-2.js'),
-  level3    : require('./level-2.3.js'),
-  level4    : require('./level-2.6.js'),
-  level5    : require('./level-5.js'),
-  level6    : require('./level-4.js'),
-  level7    : require('./level-3.js'),
-  level8    : require('./level-6.js'),
-  level9    : require('./level-7.js')
+  level1    : require('./level-0.js'),
+  level2    : require('./nicklevel-1.js'),
+  level3    : require('./level-2.js'),
+  level4    : require('./level-2.3.js'),
+  level5    : require('./level-2.6.js'),
+  level6    : require('./level-5.js'),
+  level7    : require('./level-4.js'),
+  level8    : require('./level-3.js'),
+  level9    : require('./level-6.js'),
+  level10    : require('./level-7.js')
 }
