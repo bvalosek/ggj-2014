48e4e87377e0f6967c54b5df94af5fa778cc34bd
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-25 19:04:29 -0600

    Level 7 vert walls

--- game/maps/level-7.js
+++ game/maps/level-7.js
@@ -12,5 +12,26 @@ module.exports = {
   gems: [
   ],
   walls: [
-].concat(line(u(5),u(5),C,u(5),colors.red,'r1'))
+].concat(line(u(3),u(2),C,u(2),colors.red,'r1')).
+concat(line(u(9),u(2),C,u(2),colors.purple,'p1')).
+concat(line(u(18),u(2),C,u(2),colors.yellow,'y1')).
+concat(line(u(24),u(2),C,u(2),colors.purple,'p2')).
+concat(line(u(3),u(6),C,u(2),colors.yellow,'y2')).
+concat(line(u(6),u(6),C,u(2),colors.red,'r2')).
+concat(line(u(15),u(6),C,u(2),colors.blue,'b1')).
+concat(line(u(21),u(6),C,u(2),colors.yellow,'y3')).
+concat(line(u(12),u(10),C,u(2),colors.yellow,'y4')).
+concat(line(u(15),u(10),C,u(2),colors.green,'g1')).
+concat(line(u(21),u(12),C,u(4),colors.purple,'p3')).
+concat(line(u(3),u(14),C,u(2),colors.yellow,'y5')).
+concat(line(u(9),u(14),C,u(2),colors.purple,'p4')).
+concat(line(u(12),u(14),C,u(2),colors.blue,'b2')).
+concat(line(u(24),u(16),C,u(4),colors.blue,'b3')).
+concat(line(u(3),u(20),C,u(4),colors.green,'g2')).
+concat(line(u(9),u(18),C,u(2),colors.red,'r3')).
+concat(line(u(15),u(18),C,u(2),colors.red,'r4')).
+concat(line(u(9),u(22),C,u(2),colors.blue,'b4')).
+concat(line(u(15),u(22),C,u(2),colors.purple,'p5'))
+
+
 };
