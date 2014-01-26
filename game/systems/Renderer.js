e2f6bdf0645e864f829346c369515fc994f1590a
Brandon Valosek <bvalosek@gmail.com>
2014-01-25 23:31:03 -0600

    gem throb based on spatials

--- game/systems/Renderer.js
+++ game/systems/Renderer.js
@@ -143,7 +143,9 @@ Renderer.prototype.drawGems = function(dt, time)
 
 Renderer.prototype.drawGem = function(entity, dt, time)
 {
-  var wiggle = Math.sin(5*time/1000) * 2 + 2;
+  var gemSize = entity.spatial.hwidth.x;
+  var mult = gemSize / 4;
+  var wiggle = Math.sin(5*time/1000) * mult + mult;
   this.screen
     .save()
     .vtranslate(entity.position.location)
