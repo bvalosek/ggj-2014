4dbb118474f82ab500cedc0c91883c344e4060a2
Nick Marra <nmarra@nick-hacintosh.gateway.2wire.net>
2014-01-26 12:08:59 -0600

    Level 2 update and reorder

--- game/maps/level-2.js
+++ game/maps/level-2.js
@@ -1,7 +1,7 @@
-var C = 10; 
+var C = 5; 
 var colors = require('./colors.js');
-var mapCX = 1000;
-var mapCY = 1000;
+var mapCX = 900;
+var mapCY = 720;
 
 var unit = mapCX/20;
 var u = require('./simplify.js').u;
@@ -12,29 +12,27 @@ var createPoly = require('./simplify.js').createPoly;
 
 module.exports = {
   levelObjects:{
-    playerStart: {x: u(1.5), y: u(23)},
-    levelFinish:{x: u(14), y: u(9)}
+    playerStart: {x: u(1), y: u(17)},
+    levelFinish:{x: u(13), y: u(8)}
   },
   size: { x: mapCX, y: mapCY },
   startColor: colors.red,
   gems: [].
 concat(createGem(u(4), u(6), colors.red, 'r1')).
-concat(createGem(u(9.5), u(5), colors.green, 'g1')).
-concat(createGem(u(9.5), u(12), colors.blue, 'b1')).
-concat(createGem(u(12), u(18), colors.green, 'g2')).
-concat(createGem(u(5), u(24), colors.green, 'g3')).
-concat(createGem(u(2), u(19), colors.blue, 'b2')).
-concat(createGem(u(23), u(16), colors.blue, 'b3'))
+concat(createGem(u(9), u(5), colors.green, 'g1')).
+concat(createGem(u(9), u(11), colors.blue, 'b1')).
+concat(createGem(u(10), u(16), colors.green, 'g2')).
+concat(createGem(u(4), u(17), colors.green, 'g3')).
+concat(createGem(u(2), u(14), colors.blue, 'b2')).
+concat(createGem(u(18), u(16), colors.blue, 'b3'))
  ,
   walls: [].
-concat( line(u(4), u(17), u(4), C, colors.blue,'b1') ).
-concat( line(u(8)-C, u(21), C, u(4), colors.blue,'b2') ).
-concat( line(u(19), u(2)-C/2, C, u(2)-C/2, colors.blue, 'b3') ).
-concat( line(u(5.5), u(8.5), u(5.5), C, colors.green, 'g1') ).
-concat( line(u(15), u(18), C, u(7), colors.green, 'g2') ).
-concat( createFullPoly(u(14), u(9), u(13), u(11), colors.red, C ) ).
-concat( createFullPoly(u(14), u(9), u(5), u(4), colors.blue, C ) ).
-concat( createPoly(u(23.5), u(8), u(3), u(3), 
-	[colors.blue,colors.blue,colors.blue,colors.blue], [1,0,1,1], C ) )
+concat( line(u(3)+C/2, u(13), u(3)+C/2, C, colors.blue,'b1') ).
+concat( line(u(6), u(15.5)-C/2, C, u(2.5)+C/2, colors.blue,'b2') ).
+concat( line(u(16), u(2)-C/2, C, u(2)-C/2, colors.blue, 'b3') ).
+concat( line(u(4), u(8.5), u(4), C, colors.green, 'g1') ).
+concat( line(u(13), u(15), C, u(3), colors.green, 'g2') ).
+concat( createFullPoly(u(13), u(8), u(10), u(8), colors.red, C ) ).
+concat( createFullPoly(u(13), u(8), u(5), u(4), colors.blue, C ) )
 };
 
--- game/maps/levels.js
+++ game/maps/levels.js
@@ -2,9 +2,9 @@ module.exports = {
   level0    : require('./level-0.js'),
   level1    : require('./nicklevel-1.js'),
   level2    : require('./level-2.js'),
-  level3    : require('./level-3.js'),
+  level3    : require('./level-5.js'),
   level4    : require('./level-4.js'),
-  level5    : require('./level-5.js'),
+  level5    : require('./level-3.js'),
   level6    : require('./level-6.js'),
   level7    : require('./level-7.js')
 }
